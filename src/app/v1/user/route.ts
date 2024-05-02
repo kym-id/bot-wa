eb8a7d6e37236c06d3814e0cf318ca4ca811363b
kym-wa <kym.it.team+railwaywa@gmail.com>
2024-05-02 12:21:10 +0000

    Can check user

---
+++ src/app/v1/user/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "~/lib/prisma";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const jid = searchParams.get("jid");
+
+  if (!jid) {
+    return NextResponse.json({
+      success: false,
+      error: `Please provide jid on search params`,
+    });
+  }
+
+  const user = await prisma.user.findUnique({ where: { jid } });
+
+  console.log(user);
+
+  return NextResponse.json({ success: true, data: user, exists: !!user });
+}
+
+export async function POST(request: NextRequest) {
+  const res = (await request.json()) as {
+    jid: string;
+    name: string;
+    pronouns: string;
+  };
+  const user = await prisma.user.upsert({
+    where: {
+      jid: res.jid,
+    },
+    create: {
+      jid: res.jid,
+      name: res.name,
+      pronouns: res.pronouns,
+    },
+    update: {
+      name: res.name,
+      pronouns: res.pronouns,
+    },
+  });
+  return NextResponse.json({ user });
+}
