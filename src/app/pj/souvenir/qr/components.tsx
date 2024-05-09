a0d356fc8df10b61e103d5f35fefa240a9be89e8
kym-wa <kym.it.team+railwaywa@gmail.com>
2024-05-09 12:24:07 +0000

    Restructure scan page

--- src/app/pj/souvenir/qr/components.tsx
+++ src/app/pj/souvenir/qr/components.tsx
@@ -2,26 +2,33 @@
 import { QrReader } from "~/components/qr-scanner";
 
 function QRScanComponent() {
+  return <QrReader />;
+}
+
+function EventDetails({
+  name,
+  totalVoucher: _totalVoucher,
+  totalRedeemed: _totalRedeemed,
+}: {
+  name: string;
+  totalVoucher: number;
+  totalRedeemed: number;
+}) {
   return (
-    <div className="flex flex-col justify-center items-center w-full md:w-6/12 mx-auto">
-      <div className="flex h-[50%]">
-        <QrReader />
-      </div>
-      <div className="mt-2 w-full flex flex-col items-center">
-        <h1 className="text-xl font-semibold my-2">Solo Mei</h1>
-        <div className="flex justify-evenly w-full">
-          <div className="rounded border border-white flex flex-col justify-center items-center p-2">
-            <span>Total Hadir</span>
-            <div className="text-4xl font-bold">90</div>
-          </div>
-          <div className="rounded border border-white flex flex-col justify-center items-center p-2">
-            <span>Penukaran</span>
-            <div className="text-4xl font-bold">10</div>
-          </div>
+    <>
+      <h1 className="text-xl font-semibold my-2">{name}</h1>
+      <div className="flex justify-evenly w-full">
+        <div className="rounded border border-white flex flex-col justify-center items-center p-2">
+          <span>Total Hadir</span>
+          <div className="text-4xl font-bold">{_totalVoucher}</div>
+        </div>
+        <div className="rounded border border-white flex flex-col justify-center items-center p-2">
+          <span>Penukaran</span>
+          <div className="text-4xl font-bold">{_totalRedeemed}</div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export { QRScanComponent };
+export { QRScanComponent, EventDetails };
--- src/app/pj/souvenir/qr/page.tsx
+++ src/app/pj/souvenir/qr/page.tsx
@@ -1,12 +1,51 @@
 import { Metadata } from "next";
-import { QRScanComponent } from "./components";
+import { EventDetails, QRScanComponent } from "./components";
+import { prisma } from "~/lib/prisma";
 
 export const metadata: Metadata = {
   title: "Scan QR penukaran souvenir",
 };
 
-function QRSouvenirScanPage() {
-  return <QRScanComponent />;
+async function QRSouvenirScanPage({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const key = searchParams["key"];
+  if (!key) {
+    return <div>Please provide key to url</div>;
+  }
+  const event = await prisma.event.findUnique({
+    where: { key: key as string },
+    include: { vouchers: true },
+  });
+  if (!event) {
+    return <div>Event with key: {key} is not found</div>;
+  }
+  if (!event.open) {
+    return <div>Event already closed</div>;
+  }
+  return (
+    <div className="flex flex-col justify-center items-center w-full md:w-6/12 mx-auto">
+      <div className="flex h-[50%]">
+        <QRScanComponent />
+      </div>
+      <div className="mt-2 w-full flex flex-col items-center">
+        <EventDetails
+          name={event.name}
+          totalVoucher={event.vouchers.length}
+          totalRedeemed={event.vouchers.reduce((p, v) => {
+            if (v.redeemAt) {
+              return p + 1;
+            }
+            return p;
+          }, 0)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default QRSouvenirScanPage;
